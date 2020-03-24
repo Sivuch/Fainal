28a4ec3194242e2b65b339035355dae3e6dc6d47
Sivuch <59908434+Sivuch@users.noreply.github.com>
2020-03-24 09:38:53 +0700

    2nd

---
+++ index.php
@@ -0,0 +1,11 @@
+<HTML>
+<HEAD><TITLE> My Homepage </TITLE></HEAD>
+<BODY BGCOLOR=#FFFFFF>
+<H1>
+<?php echo "รหัสนิสิต-62310095"; ?><br>
+<?php echo "นาย ศิวัช ทัพขวา"; ?><br>
+<iframe width="500" height="500"
+src="">
+</iframe>
+</BODY>
+</HTML>
