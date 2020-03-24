967db1abae01cc48397b80e1cacdd90970b20e3b
Sivuch <59908434+Sivuch@users.noreply.github.com>
2020-03-24 10:32:59 +0700

    6th

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 <?php echo "รหัสนิสิต-62310095"; ?><br>
 <?php echo "นาย ศิวัช ทัพขวา"; ?><br>
 <?php echo "######วันสอบมหาลัยที่เเสนสนุก######"; ?><br>
-<iframe width="500" height="500"
-src="https://youtu.be/AgaENpQq8Ng">
+<iframe width="560" height="315" 
+src="https://www.youtube.com/embed/AgaENpQq8Ng" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
 </iframe>
 </BODY>
 </HTML>
